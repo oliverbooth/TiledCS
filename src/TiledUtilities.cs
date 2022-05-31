f11924d63893d5ee5652397805706af3ba1901a4
Oliver Booth <me@olivr.me>
2022-05-31 16:12:01 +0100

    Add missing TiledUtilities for hex color translation

---
+++ src/TiledUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Globalization;
+
+namespace TiledCS;
+
+internal static class TiledUtilities
+{
+    public static Color HexToColor(string hex)
+    {
+        if (string.IsNullOrWhiteSpace(hex)) return Color.Transparent;
+
+        if (hex[0] == '#') hex = hex[1..];
+
+        byte r = byte.Parse(hex[..2], NumberStyles.HexNumber);
+        byte g = byte.Parse(hex[2..4], NumberStyles.HexNumber);
+        byte b = byte.Parse(hex[4..6], NumberStyles.HexNumber);
+
+        return Color.FromArgb(r, g, b);
+    }
+}
